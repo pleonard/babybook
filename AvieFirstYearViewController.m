a799d53d3aea2538cb275298064858515081d1e8
Patrick Leonard <pleonard@healthagen.net>
2012-11-23 14:12:48 -0700

    updated landscape images. the full size landscape jpg images weren't working in UIImageView. I exported the image in iPhoto and changed the size to Large (quality maximum) and they work great now. a few other text changes on the descriptions

--- AvieFirstYearViewController.m
+++ AvieFirstYearViewController.m
@@ -41,11 +41,11 @@
     _avieImageMarch.image = [UIImage imageNamed:@"avie_burrito.jpg"];
     _avieImageApril.image = [UIImage imageNamed:@"apr_feeding.jpg"];
     _avieImageMay.image = [UIImage imageNamed:@"may_bath.jpg"];
-    _avieImageJune.image = [UIImage imageNamed:@"june_dillon.jpg"];
-    _avieImageJuly.image = [UIImage imageNamed:@"jul_hiking.jpg"];
-    _avieImageAugust.image = [UIImage imageNamed:@"aug_hair.jpg"];
+    _avieImageJune.image = [UIImage imageNamed:@"june_dillon_large.jpg"];
+    _avieImageJuly.image = [UIImage imageNamed:@"jul_hiking_large.jpg"];
+    _avieImageAugust.image = [UIImage imageNamed:@"aug_hair_large.jpg"];
     _avieImageSeptember.image = [UIImage imageNamed:@"sep_laugh_yard.jpg"];
-    _avieImageOctober.image = [UIImage imageNamed:@"oct_lobster.jpg"];
+    _avieImageOctober.image = [UIImage imageNamed:@"oct_lobster_large.jpg"];
     _avieImageNovember.image = [UIImage imageNamed:@"nov_bath_mohawk.jpg"];
 
     //March description
@@ -53,18 +53,18 @@
     
     // create array of images for use in detail view controller
     _avieMonthTitles = @[@"March", @"April", @"May", @"June", @"July", @"August", @"September", @"October", @"November", @"December", @"January", @"February"];
-    _avieImages = @[@"avie_burrito.jpg", @"apr_feeding.jpg", @"may_bath.jpg", @"june_dillon.jpg", @"jul_hiking.jpg", @"aug_hair.jpg", @"sep_laugh_yard.jpg", @"oct_lobster.jpg", @"nov_bath_mohawk.jpg"
+    _avieImages = @[@"avie_burrito.jpg", @"apr_feeding.jpg", @"may_bath.jpg", @"june_dillon_large.jpg", @"jul_hiking_large.jpg", @"aug_hair_large.jpg", @"sep_laugh_yard.jpg", @"oct_lobster_large.jpg", @"nov_bath_mohawk.jpg"
     ];
     _avieDescriptions = @[
         @"After a long day of Mommy working very, very, hard, our little Avery was born on a snowy day the 1st of March 2012. When she rolled over after only 2 weeks we knew she would be a ""spirited"" little girl!",
         @"Avie looking up at Daddy. There is absolutely nothing like the first time that your little girl looks up at you.",
-        @"""Tonight is baaaath night... we're gonna have a paaaarty! A party in the baaaathtub. Cause tonight is baaaath night!"" Mommy wrote the best bath night song ever and Avie loves bath night.",
-        @"We learned quickly that Avie loves being outside - she's definitely a Colorado girl. We went to Dillon with Noni and Pawpaw and looked up at the trees and clouds.",
-        @"We got the ""Bob"" stroller for a reason - to take off road! So here is Mommy and Avie on the Tenderfoot trail near the condo in Dillon. She doesn't like being in the stroller, but is perfectly happy with it if she's on a hike in the mountains.",
+        @"'Tonight is baaaath night... we're gonna have a paaaarty! A party in the baaaathtub. Cause tonight is baaaath night!' Mommy wrote our bath night song and Avie loves bath night.",
+        @"We learned quickly that Avie loves being outside - she's definitely a Colorado girl. We went to Dillon with Noni and Papaw and looked up at the trees and clouds.",
+        @"We got the 'Bob' stroller for a reason - to take off road! So here is Mommy and Avie on the Tenderfoot trail near the condo in Dillon. She doesn't like being in the stroller, but is perfectly happy with it if she's on a hike in the mountains.",
         @"Avie's hair became kind of famous for sticking straight up on it's own. Daddy called it her '80's punk rock do'. As her hair got longer it started to lay down and she traded her spikes in for a side ponytail.",
         @"Soon after she started to smile, she started to laugh. Both are infectous - when Avie laughs you can't help but laugh too.",
-        @"Mommy had been talking about dressing up Avie as a lobster in a pot for Halloween for a long time. She wasn't kidding. Not sure Avie loved it but she did look cute. Here's Avie and Kalyn before we went to the Gaylord St. festival with Des & Bri.",
-        @"Avie still loves bath night. And Mommy and Daddy think the mohawk is pretty hilarious."
+        @"Mommy had been talking about dressing up Avie as a lobster in a pot for Halloween for a long time. She wasn't kidding. Not sure Avie loved it but she did look cute. Here are Avie and Kalyn before we went to the Gaylord St. festival with Des & Bri.",
+        @"Avie still loves bath night! And Mommy and Daddy think the mohawk is pretty hilarious."
     ];
 
     // Uncomment the following line to preserve selection between presentations.
